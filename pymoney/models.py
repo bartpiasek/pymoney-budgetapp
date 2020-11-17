09e3419766055b4f87b453029e21a01dc3ca61a3
Bart Piasek <bartpiasek@icloud.com>
2020-11-17 19:46:10 +0100

    CONFIG: models, admin.py

--- pymoney/models.py
+++ pymoney/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Budget(models.Model):
+    """
+    docstring
+    """
+    name = models.CharField(max_length=100)
+    budget = models.IntegerField()
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Budget, self).save(*args, **kwargs)
+
+
+class Category(models.Model):
+    """
+    docstring
+    """
+    Budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
+class Expense(models.Model):
+    """
+    docstring
+    """
+    Budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Income(models.Model):
+    """
+    docstring
+    """
+    Budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Saving(models.Model):
+    """
+    docstring
+    """
+    Budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
